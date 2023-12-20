5470bd5e0a4900b646d7c0b0bca4f8e9a29c1355
CiaoNw <y.naruto321@gmail.com>
2023-12-20 15:04:56 +0100

    first commit

---
+++ animation/index/index copy.js
@@ -0,0 +1,20 @@
+let x = 0;
+function onLoad(e){
+    var a = document.getElementById('test');
+    h=x-a.scrollTop;
+    if (h<0){
+        document.getElementById('navbar').style.width="82px";
+        document.getElementById('momo').style.opacity="0";
+        x=a.scrollTop;
+    }else{
+        document.getElementById('navbar').style.width="95%";
+        document.getElementById('momo').style.opacity="1";
+        x=a.scrollTop;
+    }
+    console.log(a.scrollTop);
+
+}
+function fleche(){
+    document.getElementById('fleche').classList.toggle('fleche2');
+}
+setInterval(fleche,500)
